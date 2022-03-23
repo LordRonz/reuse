35ccb39216247af7c9c524d48e85f889b30c666f
LordRonz <aarontanhar2000@gmail.com>
2022-03-23 21:44:57 +0700

    feat: add risaikuru logo :rocket:

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,12 @@
 /* eslint-disable @next/next/no-img-element */
 import type { NextPage } from 'next';
+import Image from 'next/image';
 
+import Accent from '@/components/Accent';
 import Layout from '@/components/layout/Layout';
 import Seo from '@/components/Seo';
+
+import risaikuru_logo from '../../public/risaikuru_trans.png';
 
 const Home: NextPage = () => {
   return (
@@ -10,8 +14,18 @@ const Home: NextPage = () => {
       <Seo />
       <main>
         <section className='bg-black'>
-          <div className='layout flex min-h-screen flex-col items-center justify-center text-center'>
-            <h1>Next.js + Tailwind CSS + TypeScript Starter</h1>
+          <div className='layout flex flex-col items-center justify-center text-center'>
+            <div>
+              <Image
+                src={risaikuru_logo}
+                alt='Risaikuru'
+                width={300}
+                height={300}
+              />
+              <h1 className='text-2xl md:text-5xl'>
+                <Accent>A web service to classify your trash</Accent>
+              </h1>
+            </div>
           </div>
         </section>
       </main>
